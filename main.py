57cf0011895e270965507134431f8d7d62c2c55f
lo-j-kee <ljkeema@calpoly.edu>
2026-04-09 19:01:03 -0700

    Commit 1

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ def welcome_message(name:str) -> str:
    return message
 
 
-message = welcome_message("anrathi@calpoly.edu")
+message = welcome_message("ljkeema@gmail.com")
+print(message)
 print(message)
